5c3871c043cf633427322ecced37fbc037b2f4a1
malwareami! <46420533+malicious-dev@users.noreply.github.com>
2022-11-13 22:58:03 +0530

    get data api

--- routes/user.js
+++ routes/user.js
@@ -6,13 +6,13 @@ router.post('/register', async (req, res) => {
   const {name, email, age, mobile, work, add, desc} = req.body;
 
   if(!name || !email || !mobile || !work || !add || !desc) {
-    return res.status(404).send("plz fill the data");
+    return res.status(404).json("plz fill the data");
   }
   try{
     const preuser = await users.findOne({email: email});
     console.log(preuser);
     if(preuser){
-      res.status(404).send("User already Exists");
+      res.status(404).json("User already Exists");
     } else {
       const addUser = await users({
         name,email,age,mobile,work,add,desc})
@@ -23,6 +23,17 @@ router.post('/register', async (req, res) => {
     }
   }catch(e){
     return res.status(404).json(e.message);
+  }
+})
+
+//get users
+
+router.get('/getdata', async (req, res) => {
+  try {
+    const userdata = await users.find();
+    res.status(200).json(userdata);
+  }catch(e){
+    res.status(404).json(e.message);
   }
 })
 
